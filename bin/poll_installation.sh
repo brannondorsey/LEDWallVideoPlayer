f5a3f9c40367a278ce48d9d821eed4a2858afd8e
danthemellowman <dan@makeitdoathing.com>
2015-09-01 02:27:41 -0700

    Update scripts and crons

--- bin/poll_installation.sh
+++ bin/poll_installation.sh
@@ -3,11 +3,14 @@
 # launches installation in background if it is not already running, 
 # then exists with 0. Does nothing if installation is running, exits with 1
 
+env DISPLAY=:0
+echo $USER
+
 BASENAME=$(dirname $0)
 
 if [ !  "$(pidof -x LEDWallVideoPlayer)" ]; then
 	echo "Installation NOT running. Launching $BASENAME/LEDWallVideoPlayer"
-	"$BASENAME/LEDWallVideoPlayer" >/dev/null 2>&1 &
+	"$BASENAME/LEDWallVideoPlayer" > ~/Desktop/test.out #>/dev/null 2>&1 &
 	exit 0
 else
 	echo "Installation running."
--- bin/start_installation.sh
+++ bin/start_installation.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-BASEDIR=$(dirname $0)
-"$BASEDIR/LEDWallVideoPlayer"
+sleep 60 # 2 minutes
+
+if [ ! "$(pidof -x LEDWallVideoPlayer)" ]; then
+	BASEDIR=$(dirname $0)
+	"$BASEDIR/LEDWallVideoPlayer" > ~/Desktop/out.txt
+fi
